19dc32ac8dbfaf2f1407f93ed4a6299a14b4e27a
ArranRoy <arranroy@linux043.computing.dundee.ac.uk>
2019-09-16 12:00:09 +0100

    first virus adding #virusadded

--- virus/src/test.sh
+++ virus/src/test.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+test= find . -name "*.sh"
+
+echo $test
+echo $(grep -c "#virusadded" *.sh)
+# if [ $(grep -c "#virusadded" $test) -eq 1 ]
+# then
+# echo "got through if"
+# else
+# echo "#virusadded" >> $test
+# fi
+
