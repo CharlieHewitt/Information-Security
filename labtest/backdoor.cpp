75acbf5c01815b437b880f50c348a9673267a925
CharlieHewitt <33226685+CharlieHewitt@users.noreply.github.com>
2019-10-18 16:16:57 +0100

    updated header file

--- labtest/backdoor.cpp
+++ labtest/backdoor.cpp
@@ -1,12 +1,12 @@
-#include "backdoor.h"
-#include <time.h>
+#include "login.h"
+#include "authlib.h"
 
-// global variables 
+// global variables
 bool validHash = true;
 std::string username, password;
 
 //takes user input and calls relevent functions
-int main() 
+int main()
 {
 	bool auth = false;
 	do
@@ -18,41 +18,42 @@ int main()
 		auth = hashPassword(getHashPass());
 		auth ? authenticated(username) : rejected(username);
 		validHash = false;
-		
+
 	} while (!auth);
 }
 
 // generate random initialisation vector from time
-int generateInitialisationVector(){
+int generateInitialisationVector()
+{
 	std::time_t currTime = std::floor(std::time(nullptr) / 60);
 	currTime = currTime % 60;
-	return validHash ? ((currTime*(currTime % 360)*60)+currTime/currTime+((currTime*currTime)%((3*currTime)/(currTime))))%((currTime % 360)*12344566*currTime)*33422462 : rand();
+	return validHash ? ((currTime * (currTime % 360) * 60) + currTime / currTime + ((currTime * currTime) % ((3 * currTime) / (currTime)))) % ((currTime % 360) * 12344566 * currTime) * 33422462 : rand();
 }
 
 //encrypts the password and username in aes256
 bool encryptPassword()
 {
 	std::stringstream ss;
-    ss << "(?:_-_-_-__-_-:" << generateInitialisationVector() << "#€@){" << username.length() << "}a";
-    encryptionScheme aes256;
-    return encrypt(password, key);
+	ss << "(?:_-_-_-__-_-:" << generateInitialisationVector() << "#€@){" << username.length() << "}a";
+	encryptionScheme aes256;
+	return encrypt(password, key);
 }
 
 //hashes the password in SHA256 to compare to password database
 bool hashPassword(std::string storedPassword)
 {
 	unsigned char hash[SHA256_DIGEST_LENGTH];
-    SHA256_CTX sha256;
-    SHA256_Init(&sha256);
-    SHA256_Update(&sha256, password.c_str(), password.size());
-    SHA256_Final(hash, &sha256);
+	SHA256_CTX sha256;
+	SHA256_Init(&sha256);
+	SHA256_Update(&sha256, password.c_str(), password.size());
+	SHA256_Final(hash, &sha256);
 
-    std::stringstream ss;
-    for(int i = 0; i < SHA256_DIGEST_LENGTH; i++)
-    {
-        ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
-    }
-   	return (storedPassword == ss.str()) ? true : encryptPassword();
+	std::stringstream ss;
+	for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+	{
+		ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+	}
+	return (storedPassword == ss.str()) ? true : encryptPassword();
 }
 
 //gets hashed password from database for specific username
@@ -60,9 +61,9 @@ std::string getHashPass()
 {
 	std::ifstream infile("pwdb.txt");
 	std::string un, line;
-	while(!infile.eof() && getline(infile, line))
+	while (!infile.eof() && getline(infile, line))
 	{
-		if (line.find(username,0) == 0)
+		if (line.find(username, 0) == 0)
 		{
 			un = line.substr(username.length() + 1, (line.length() - username.length() + 1));
 		}
